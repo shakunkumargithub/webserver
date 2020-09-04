e1a30ed2684effc8074786a737b886506707a86f
Kumar <shakkum@apac.corpdir.net>
2020-09-05 00:11:29 +0530

    Adding Web development project

---
+++ server.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)
+print (__name__)
+
+
+@app.route('/')
+def my_home():
+    return render_template('index.html')
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method=='POST':
+        data = request.form.to_dict()
+        write_to_file(data)
+        return redirect('/thankyou.html')
+    else:
+        return 'something went wrong. try again!'
+
+
+def write_to_file(data):
+    with open('database.txt',mode='a') as database:
+        email=data["email"]
+        subject=data["subject"]
+        message = data["message"]
+        file=database.write(f'\n{email},{subject},{message}')
+
